b4fcfb22f476af87a1f9872b733a63012f3342ac
Urban Cvek <urban.cvek@shopster.io>
2017-03-04 22:42:31 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,37 @@
+const morganToken = (req) => {
+  const { query } = req.body;
+  if (!query) return '\n';
+  try {
+    const ast = gql`${query}`;
+    if (!ast) return '\n';
+
+    const queries = ast.definitions[0].selectionSet.selections.map(sel => ({
+      name: sel.name.value,
+      args: sel.arguments.map(arg => ({
+        name: arg.name.value,
+        value: arg.value.value,
+      })),
+    }));
+
+    let text = '\n';
+
+    queries.forEach(({ name, args }) => {
+      text += `\t${name}`;
+      if (args.length === 0) return text += '\n';
+      text += ' (';
+
+      args.forEach((arg, i) => {
+        if (i !== 0) text += ', ';
+        text += `${arg.name}: ${arg.value}`;
+      });
+
+      return text += ')\n';
+    });
+
+    return text;
+  } catch (error) {
+    return '\n';
+  }
+};
+
+export default morganToken;
